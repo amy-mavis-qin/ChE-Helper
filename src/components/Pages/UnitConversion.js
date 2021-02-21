50d51a6ee141ebfaa0aac80f4c5311c67e384209
amy-mavis-qin <amyqin501@gmail.com>
2021-02-21 16:07:19 -0500

    Added Unit Conversion page

--- src/components/Pages/UnitConversion.js
+++ src/components/Pages/UnitConversion.js
@@ -1,17 +1,124 @@
-import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import React, { Component } from 'react';
+import SelectSearch from 'react-select-search';
+import { antoine_constants } from './antoine_constants.js'
+import { unitConvert, tempConvert } from "../../scripts/functions.js"
 
-function UnitConversion() {
-    return (
-        <>
-            <h1 className="mainTitle">Work In Progress</h1>
-        </>
-    )
+class UnitConversion extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            value: "",
+            unitVariableInput: "",
+            unitVariable:"",
+            tempInput: "",
+            tempInputUnit:"",
+            unitDisabled:true,
+            tempDisabled:true,
+            resultUnit:"",
+            resultTemp:"",
+        }
+        this.handleConvertUnit=this.handleConvertUnit.bind(this)
+        this.handleConvertTemp=this.handleConvertTemp.bind(this)
+        this.handleInputChange=this.handleInputChange.bind(this)
+        this.checkIfUnitReady=this.checkIfUnitReady.bind(this)
+        this.checkIfTempReady=this.checkIfTempReady.bind(this)
+    }
+    handleInputChange = (param, e) => {
+        this.setState({ [param]: e.target.value })
+        if(this.checkIfTempReady()){
+            this.setState({tempDisabled:false})
+        } else if(this.checkIfUnitReady()){
+            this.setState({unitDisabled:false})
+        }
+    }     
+    checkIfUnitReady = () => {
+        if(this.state.unitVariableInput !== "" && this.state.unitVariable !== ""){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    checkIfTempReady = () => {
+        if(this.state.tempInput !== "" && this.state.tempInputUnit !== ""){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    handleConvertUnit = () => {
+        this.setState({resultUnit:unitConvert(this.state.unitVariableInput, this.state.unitVariable)})
+        console.log(unitConvert(this.state.unitVariableInput, this.state.unitVariable))
+    }
+    handleConvertTemp = () => {
+        this.setState({resultTemp:tempConvert(this.state.tempInput, this.state.tempInputUnit)})
+    }
+
+    render() {
+        return(
+            <div>
+                <div className='div-container'>
+                    <div className="container-left">
+                        Unit Conversion
+                    </div>
+                    <div className="container-right">
+                        Supported Conversion: Length, Area, Volume, Mass, Density, Force, Pressure, Energy, Power, Time
+                        <div className='unit-conversion'>
+                        <br />
+                            <input className='unit-variable-input' placeholder="Input Value (Ex. 1min)" onChange={e => {
+                                this.setState({unitVariableInput: e.target.value}, () =>{
+                                    if(this.checkIfUnitReady()){
+                                        this.setState({unitDisabled:false})
+                                    }
+                                })
+                            }
+                            }/>
+                            <input className='unit-input' placeholder="Target Unit (Ex. sec)" onChange={e => {
+                                this.setState({unitVariable: e.target.value}, () =>{
+                                    if(this.checkIfUnitReady()){
+                                        this.setState({unitDisabled:false})
+                                    }
+                                })
+                            }
+                            }/>
+                            <button className="submit-button" onClick={this.handleConvertUnit} disabled={this.state.unitDisabled}>Calculate</button>
+                        </div>
+                        <div className="answer">
+                            {this.state.resultUnit}
+                        </div>
+                    </div>
+                </div>
+                <div className='div-container'>
+                    <div className="container-left">
+                        Temperature Conversion
+                    </div>
+                    <div className="container-right">
+                    <div className='temp-conversion'>
+                        <input className='unit-variable-input' placeholder="Input Value (Ex. 30C)" onChange={e => {
+                                this.setState({tempInput: e.target.value}, () => {
+                                    if(this.checkIfTempReady()){
+                                        this.setState({tempDisabled:false})
+                                    }
+                                })
+                            }
+                            }/>
+                            <input className='unit-input' placeholder="Target Unit (Ex. F)" onChange={e => {
+                                this.setState({tempInputUnit: e.target.value}, () => {
+                                    if(this.checkIfTempReady()){
+                                        this.setState({tempDisabled:false})
+                                    }
+                                })
+                            }
+                            }/>
+                            <button className="submit-button" onClick={this.handleConvertTemp} disabled={this.state.tempDisabled}>Calculate</button>
+                        </div>
+                        <div className="answer">
+                            {this.state.resultTemp}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )    
+    }
 }
-
-export default UnitConversion
+ 
+export default UnitConversion;
--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -2,67 +2,83 @@ var units = require('../definitions/units.js');
 units = units.units;
 
 function variableInput() {
-    var parsedObj = {}
-    var k=1;
-    for (i=0;i<arguments.length;i++){
-        var knownValue = parseFloat((arguments[i].match(/\d+/))[0], 10);
-        var unit = arguments[i].replace(knownValue,"").toLowerCase();
-        var unitObj = Object.entries(units)
-        for(var i=0; i<(unitObj.length); i++){
-            if(unit in unitObj[i][1] && !parsedObj[unitObj[i][0]]){
-                parsedObj[unitObj[i][0]]=[knownValue,unit];
-                break;
-            } else if (unit in unitObj[i][1] && parsedObj[unitObj[i][0]]){
-                parsedObj[unitObj[i][0]+'_'+k]=[knownValue,unit];
-                k++;
-            }  
+    try{
+        var parsedObj = {}
+        var k=1;
+        for (i=0;i<arguments.length;i++){
+            var knownValue = parseFloat((arguments[i].match(/\d+/))[0], 10);
+            var unit = arguments[i].replace(knownValue,"").toLowerCase();
+            var unitObj = Object.entries(units)
+            for(var i=0; i<(unitObj.length); i++){
+                if(unit in unitObj[i][1] && !parsedObj[unitObj[i][0]]){
+                    parsedObj[unitObj[i][0]]=[knownValue,unit];
+                    break;
+                } else if (unit in unitObj[i][1] && parsedObj[unitObj[i][0]]){
+                    parsedObj[unitObj[i][0]+'_'+k]=[knownValue,unit];
+                    k++;
+                }  
+            };
         };
-    };
-    return(parsedObj);
+        return(parsedObj);
+    } catch (e) {
+        if(e instanceof TypeError){
+            alert("Invalid Input")
+        }
+    }
 };
 
 function unitConvert(input,outputUnit){
-    var inputValue, inputUnit, convertedObj;
-    if(!typeof input === "object"){
-        inputValue = Object.values(variableInput(input))[0][0];
-        inputUnit = Object.values(variableInput(input))[0][1];
-    } else {
-        inputValue = input[0];
-        inputUnit = input[1];
-    }
-    for(var i=0; i<(Object.entries(units).length); i++){
-        var properties = Object.entries(units)[i][1]
-        if (properties[inputUnit] && properties[outputUnit]){
-            var convertedValue = inputValue* (properties[outputUnit]/properties[inputUnit]);
-            return(convertedObj={convertedValue,outputUnit})
+    try{
+        var inputValue, inputUnit;
+        input = Object.values(variableInput(input))
+        inputValue = input[0][0];
+        inputUnit = input[0][1];
+        inputUnit = inputUnit.toLowerCase();
+        outputUnit = outputUnit.toLowerCase();    
+        for(var i=0; i<(Object.entries(units).length); i++){
+            var properties = Object.entries(units)[i][1]
+            if (properties[inputUnit] && properties[outputUnit]){
+                var convertedValue = inputValue* (properties[outputUnit]/properties[inputUnit]);
+                return("The converted value is "+ convertedValue+" "+outputUnit);
+            };
         };
-    };
+    } catch (e) {
+        if(e instanceof TypeError){
+            alert("Invalid Input")
+        }
+    }
 };
 
 function tempConvert(input,outputUnit){
-    var inputValue, inputUnit, convertedValue;
-    if(typeof input === "string"){
-        inputValue = parseFloat((input.match(/\d+/))[0], 10);
-        inputUnit = input.replace(inputValue,"").toLowerCase();
-    } else {
-        inputValue = input[0];
-        inputUnit = input[1];
-    };
-    if(inputUnit == 'c' && outputUnit == 'f'){
-        convertedValue = (inputValue * (9/5)) + 32;
-    } else if (inputUnit == 'f' && outputUnit == 'c'){
-        convertedValue = (inputValue - 32) * (5/9); 
-    } else if (inputUnit == 'c' && outputUnit == 'k'){
-        convertedValue = inputValue + 273;
-    } else if (inputUnit == 'k' && outputUnit == 'c'){
-        convertedValue = inputValue - 273
-    } else if (inputUnit == 'f' && outputUnit == 'k'){
-        convertedValue = (inputValue + 459.67) * (5/9)
-    } else if (inputUnit == 'k' && outputUnit == 'f'){
-        convertedValue = (inputValue * (9/5)) - 459.67
-    };
-    return convertedValue
-}
+    try{
+        var inputValue, inputUnit, convertedValue;
+        if(typeof input === "string"){
+            inputValue = parseFloat((input.match(/\d+/))[0], 10);
+            inputUnit = input.replace(inputValue,"").toLowerCase();
+        } else {
+            inputValue = input[0];
+            inputUnit = input[1];
+        };
+        if(inputUnit == 'c' && outputUnit == 'f'){
+            convertedValue = (inputValue * (9/5)) + 32;
+        } else if (inputUnit == 'f' && outputUnit == 'c'){
+            convertedValue = (inputValue - 32) * (5/9); 
+        } else if (inputUnit == 'c' && outputUnit == 'k'){
+            convertedValue = inputValue + 273;
+        } else if (inputUnit == 'k' && outputUnit == 'c'){
+            convertedValue = inputValue - 273
+        } else if (inputUnit == 'f' && outputUnit == 'k'){
+            convertedValue = (inputValue + 459.67) * (5/9)
+        } else if (inputUnit == 'k' && outputUnit == 'f'){
+            convertedValue = (inputValue * (9/5)) - 459.67
+        };
+        return ("The temperature is " + convertedValue + outputUnit)
+    } catch (e) {
+        if(e instanceof TypeError){
+            alert("Invalid Input")
+        }
+    }
+};
 
 module.exports = {
     variableInput,
